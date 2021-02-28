b914af436608b2660ac781dcb854a5c59c1e8bd4
Chad Conley <canddx3@gmail.com>
2021-02-27 19:03:10 -0600

    Delete ExpL06HandsOn/express_lesson_six directory

--- ExpL06HandsOn/express_lesson_six/sequelize-auto.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\sequelize-auto\bin\sequelize-auto" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\sequelize-auto\bin\sequelize-auto" %*
-)
